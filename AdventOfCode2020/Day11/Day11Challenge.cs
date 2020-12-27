6f96142d723578b8ad541e77cf536a39a9f9d11a
Christy Grimaud <christy.grimaud@gmail.com>
2020-12-26 22:49:46 -0500

    completed day 11 pt2

--- AdventOfCode2020/Day11/Day11Challenge.cs
+++ AdventOfCode2020/Day11/Day11Challenge.cs
@@ -48,8 +48,6 @@ namespace AdventOfCode2020.Day11
 
             int rows = rowLayout.Count;
             int columns = rowLayout[0].Length;
-            
-            List<string> systemFailure = null;
 
             List<string> currentLayout = rowLayout;
             List<string> nextLayout = new List<string>();
@@ -66,7 +64,8 @@ namespace AdventOfCode2020.Day11
                     if (seatIsEmpty || seatIsOccupied)
                     {
                         int[] location = { y, x };
-                        var surroundingLocations = GetAdjValues(rowLayout, location);
+                        //var surroundingLocations = GetAdjValues(rowLayout, location);
+                        var surroundingLocations = GetNewAdjValues(rowLayout, location);
                         //int emptySeats = 0;
                         int surroundingOccupiedSeats = 0;
                         if (surroundingLocations.ContainsKey(occupied)) surroundingOccupiedSeats = surroundingLocations[occupied];
@@ -74,7 +73,7 @@ namespace AdventOfCode2020.Day11
                         {
                             nextRow.Append(occupied);
                         }
-                        else if (seatIsOccupied && surroundingOccupiedSeats > 3)
+                        else if (seatIsOccupied && surroundingOccupiedSeats > 4)
                         {
                             nextRow.Append(empty);
                         } else
@@ -103,11 +102,7 @@ namespace AdventOfCode2020.Day11
             else
             {
                 Console.WriteLine($"# of iterations {iterations}, final number of seats: {seatCount}");
-            }
-
-            
-            
-            
+            }            
         }
 
         public static Dictionary<char, int> GetAdjValues(List<string> layout, int[] location)
@@ -164,6 +159,222 @@ namespace AdventOfCode2020.Day11
             else return true;
 
 
+        }
+
+        public static Dictionary<char, int> GetNewAdjValues(List<string> layout, int[] location)
+        {
+            var characters = new List<char>();
+            var charDictionary = new Dictionary<char, int>();
+
+            characters.Add(GetNextNorthWest(layout, location));
+            characters.Add(GetNextNorth(layout, location));
+            characters.Add(GetNextNorthEast(layout, location));
+            characters.Add(GetNextEast(layout, location));
+            characters.Add(GetNextSouthEast(layout, location));
+            characters.Add(GetNextSouth(layout, location));
+            characters.Add(GetNextSouthWest(layout, location));
+            characters.Add(GetNextWest(layout, location));
+
+
+            // TODO: can this be refactored into ternary?
+            foreach (var ch in characters)
+            {
+                if (charDictionary.ContainsKey(ch))
+                {
+                    charDictionary[ch]++;
+                }
+                else
+                {
+                    charDictionary.Add(ch, 1);
+                }
+            }
+
+            return charDictionary;
+        }
+
+
+        public static char GetNextNorthWest(List<string> layout, int[] currentLocation)
+        {
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow - 1;
+            var nCol = cCol - 1;
+
+            bool northWestValidation = cRow > 0 && cCol > 0;
+
+            if (northWestValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextNorthWest(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
+        }
+
+        public static char GetNextNorth(List<string> layout, int[] currentLocation)
+        {
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow - 1;
+            var nCol = cCol;
+
+            bool northValidation = cRow > 0;
+
+            if (northValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextNorth(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
+        }
+
+        public static char GetNextNorthEast(List<string> layout, int[] currentLocation)
+        {
+            var colLength = layout[0].Length - 1;
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow - 1;
+            var nCol = cCol + 1;
+
+            bool northEastValidation = cRow > 0 && cCol < colLength;
+
+            if (northEastValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextNorthEast(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
+        }
+
+        public static char GetNextEast(List<string> layout, int[] currentLocation)
+        {
+            var colLength = layout[0].Length - 1;
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow;
+            var nCol = cCol + 1;
+
+            bool eastValidation = cCol < colLength;
+
+            if (eastValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextEast(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
+        }
+
+        public static char GetNextSouthEast(List<string> layout, int[] currentLocation)
+        {
+            var rowLength = layout.Count - 1;
+            var colLength = layout[0].Length - 1;
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow + 1;
+            var nCol = cCol + 1;
+
+
+            bool southEastValidation = cRow < rowLength && cCol < colLength;
+
+            if (southEastValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextSouthEast(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
+        }
+
+        public static char GetNextSouth(List<string> layout, int[] currentLocation)
+        {
+            var rowLength = layout.Count - 1;
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow + 1;
+            var nCol = cCol;
+
+            bool southValidation = cRow < rowLength;
+
+            if (southValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextSouth(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
+        }
+
+        public static char GetNextSouthWest(List<string> layout, int[] currentLocation)
+        {
+            var rowLength = layout.Count - 1;
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow + 1;
+            var nCol = cCol - 1;
+
+            bool southWestValidation = cRow < rowLength && cCol > 0;
+
+
+            if (southWestValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextSouthWest(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
+        }
+
+        public static char GetNextWest(List<string> layout, int[] currentLocation)
+        {
+            var cRow = currentLocation[0];
+            var cCol = currentLocation[1];
+            var nRow = cRow;
+            var nCol = cCol - 1;
+
+            bool westValidation = cCol > 0;
+
+            if (westValidation)
+            {
+                char nextValue = layout[nRow][nCol];
+                if (nextValue == '.')
+                {
+                    int[] nextLocation = { nRow, nCol };
+                    return GetNextWest(layout, nextLocation);
+                }
+                else return nextValue;
+            }
+            else return ' ';
         }
     }
 
